674cda9378b6a8059e771c6af7ac4e58ce63e134
chuan-1080ti <chuan-1080ti@zwgroup.nju>
2021-05-07 20:49:30 +0800

    update

--- test_tvsnet.py
+++ test_tvsnet.py
@@ -1,53 +1,58 @@
 import os
 import numpy as np
+from skimage import io as skio
+
 import torch
 
-from particle.utils.dirty import loadNnData
-from particle.nn.tvsnet import VaeTVSNet, getProjections
+from mayavi import mlab
+# from particle.mayaviOffScreen import mlab
+from particle.nn.tvsnet import VaeTVSNet
 
 
-def study():
-    # from mayavi import mlab
-    from particle.mayaviOffScreen import mlab
-    from skimage import io as skio
+def loadMoel():
     model = VaeTVSNet("./particle/nn/config/tvsnet-vae-deep.xml")
-    state_dict = torch.load('/home/chuan/soil/output/tvsnet/vae/网络加深/dropout0.2, 数据增强/精调-lr0.0001-rotate60/state_dict-overfit.pt',
+    state_dict = torch.load('/home/chuan/soil/output/tvsnet/vae/网络加深/dropout0.5/state_dict-overfit.pt',
                             map_location=torch.device('cpu'))
     model.load_state_dict(state_dict)
     model.eval()
+    return model
 
-    def test_generate(ind):
-        cwd = os.getcwd()
-        os.chdir('./data/简单几何图形')
-        filenames = sorted(os.listdir())
-        filename = filenames[ind]
-        print(filename)
-        image = skio.imread(filename, as_gray=True)
-        image[image == 255] = 1
-        print(image.max())
-        os.chdir(cwd)
-        x = torch.empty((3, 64, 64), dtype=torch.uint8)
-        for i in range(3):
-            x[i] = torch.from_numpy(image)
-        model.generate(x)
-        mlab.outline()
-        mlab.axes()
-        mlab.show()
-        mlab.savefig(filename)
-        return
 
-    test_generate(4)
-    # source_set = loadNnData("data/liutao/v1/particles.npz", "testSet")
-    # projection_set = getProjections(source_set)
-    # for ind in range(200, 300):
-    #     fig1, fig2 = model.contrast(
-    #         projection_set[ind], source_set[ind, 0], voxel=True)
-    #     mlab.show()
-    # # mlab.savefig("contrast1.png", figure=fig1)
-    # # mlab.savefig("contrast2.png", figure=fig2)
-
+def testSimpleGeometry(ind, model=None):
+    cwd = os.getcwd()
+    os.chdir('./data/简单几何图形')
+    filenames = sorted(os.listdir())
+    filename = filenames[ind]
+    print(filename)
+    image = skio.imread(filename, as_gray=True)
+    image[image == 255] = 1
+    print(image.max())
+    os.chdir(cwd)
+    x = torch.empty((3, 64, 64), dtype=torch.uint8)
+    for i in range(3):
+        x[i] = torch.from_numpy(image)
+    model.generate(x)
+    mlab.outline()
+    mlab.axes()
+    mlab.show()
+    mlab.savefig(filename)
     return
 
 
+def testGenerate(model):
+    src = [skio.imread(f"{i+1}.png", as_gray=True) for i in range(3)]
+    src = [src[2], src[0], src[1]]
+    src = np.concatenate([view[np.newaxis, :, :] for view in src], axis=0)
+    src = torch.from_numpy(src)
+    src[src == 255] = 1
+    print(src.max(), src.shape)
+    model.generate(src)
+    mlab.outline()
+    mlab.axes()
+    mlab.show()
+
+
 if __name__ == "__main__":
-    study()
+    model = loadMoel()
+    # testSimpleGeometry(4, model)
+    testGenerate(model)
