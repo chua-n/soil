9d9a21d68d3fcf79f2b93639514c841537d62a54
chuan-1080ti <chuan-1080ti@zwgroup.nju>
2021-05-28 17:23:20 +0800

    update

--- test_tvsnet.py
+++ test_tvsnet.py
@@ -6,12 +6,16 @@ import torch
 
 from mayavi import mlab
 # from particle.mayaviOffScreen import mlab
-from particle.nn.tvsnet import VaeTVSNet
+from particle.pipeline import Sand
+from particle.utils.dirty import loadNnData
+from particle.nn.tvsnet import VaeTVSNet, TVSNet, getProjections, generateOneParticle, contrast
 
 
 def loadMoel():
-    model = VaeTVSNet("./particle/nn/config/tvsnet-vae-deep.xml")
-    state_dict = torch.load('/home/chuan/soil/output/tvsnet/vae/网络加深/dropout0.5/state_dict-overfit.pt',
+    model = VaeTVSNet(
+        "/home/chuan/soil/particle/nn/config/tvsnet-vae-deep.xml")
+    # model = TVSNet("/home/chuan/soil/particle/nn/config/tvsnet.xml")
+    state_dict = torch.load("/home/chuan/soil/output/tvsnet/vae/网络加深/dropout0.2, 数据增强/精调-rotate0/state_dict-overfit.pt",
                             map_location=torch.device('cpu'))
     model.load_state_dict(state_dict)
     model.eval()
@@ -39,20 +43,57 @@ def testSimpleGeometry(ind, model=None):
     return
 
 
-def testGenerate(model):
-    src = [skio.imread(f"{i+1}.png", as_gray=True) for i in range(3)]
+def loadMyPicture(folder="trial/v1"):
+    src = [skio.imread(f"/home/chuan/老板要求/chazhi/4-{i+1}.png",
+                       as_gray=True) for i in range(3)]
     src = [src[2], src[0], src[1]]
     src = np.concatenate([view[np.newaxis, :, :] for view in src], axis=0)
     src = torch.from_numpy(src)
     src[src == 255] = 1
     print(src.max(), src.shape)
-    model.generate(src)
-    mlab.outline()
-    mlab.axes()
-    mlab.show()
+    return src
+
+
+def testGenerate(src, model, **kwargs):
+    particle = generateOneParticle(model, src)
+    sand = Sand(particle)
+    from skimage.util import random_noise
+    sand.cube = random_noise(
+        sand.cube, mode="gaussian", var=0.02, seed=314)
+    print("已添加噪声！")
+    fig = sand.visualize(**kwargs)
+    return fig
 
 
 if __name__ == "__main__":
     model = loadMoel()
+    # 任务1
     # testSimpleGeometry(4, model)
-    testGenerate(model)
+
+    # 任务2
+    src = loadMyPicture(folder="/home/chuan/老板要求/liuyu")
+    # src2 = loadMyPicture(folder="v4")
+    fig = mlab.figure()
+    testGenerate(src, model, figure=fig, color=(0.7, 0.7, 0.7))
+    # testGenerate(src2, model, figure=fig, color=(0, 0.6, 0.85), opacity=0.9)
+    # mlab.outline()
+    # axes = mlab.axes(xlabel='', ylabel='', zlabel='')
+    # axes.label_text_property.font_family = "times"
+    mlab.show()
+
+    # 任务3
+    # particles = loadNnData("/home/chuan/soil/data/liutao/v1/particles.npz",
+    #                        "testSet")
+    # src = getProjections(particles)
+    # for ind in [944, 983, 1218, 1340, 1427]:
+    #     testGenerate(src[ind-1], model)
+    #     mlab.savefig(f"/home/chuan/{ind}.png")
+    #     mlab.savefig(f"/home/chuan/{ind}.obj")
+    #     mlab.close(all=True)
+
+    # 任务4
+    # ind = 1426
+    # testGenerate(src[ind], model)
+    # print(src[ind].shape, particles[ind].shape)
+    # model.contrast(src[ind], particles[ind, 0])
+    # mlab.show()
